27be5b712ae78ceeed58548d4428338bd0847b8d
Софья Андреева <skandreeva@edu.hse.ru>
2020-04-22 16:25:16 +0300

    fix

---
+++ cmake-build-debug/CMakeFiles/DamerauLevensteinDistance.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DamerauLevensteinDistance.dir/main.cpp.obj"
+  "DamerauLevensteinDistance.pdb"
+  "DamerauLevensteinDistance.exe"
+  "DamerauLevensteinDistance.exe.manifest"
+  "libDamerauLevensteinDistance.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DamerauLevensteinDistance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
